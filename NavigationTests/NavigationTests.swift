81924098439971a36f92bca7e3c33cf06a77c389
Hana Ri <hanacokeeri76@gmail.com>
2025-07-24 09:25:05 -0700

    Initial Commit

---
+++ NavigationTests/NavigationTests.swift
@@ -0,0 +1,16 @@
+//
+//  NavigationTests.swift
+//  NavigationTests
+//
+//  Created by Scholar on 7/24/25.
+//
+
+import Testing
+
+struct NavigationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
