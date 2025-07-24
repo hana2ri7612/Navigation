967a439d9f746db61b5053e239bb8d4e4c596538
Hana Ri <hanacokeeri76@gmail.com>
2025-07-24 09:43:40 -0700

    working navigation link

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,8 +12,14 @@ struct ContentView: View {
         NavigationStack{
             VStack{
                 Text("This is the root view!")
-                NavigationLink(destination: Text ("Congratulations! You've arrived in second veiw!")){
-                    Text("Click here!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                    .foregroundColor(Color(hue: 0.661, saturation: 0.979, brightness: 0.541))
+                NavigationLink(destination: Text ("Congratulations! You've arrived in second view!")
+                    .font(.title)
+                    .fontWeight(.semibold)){
+                    Text("Click me for a second view!")
+                    
                 
                 }
                 
